d5abb0627e90467c38bc486c4da0481744e6d1bf
Damian Wujec <wujecdamian12@gmail.com>
2026-04-25 00:03:36 +0200

    feat: replace App component with Greeting and GreetingImage components

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import Greeting from './Greeting.jsx'
+import GreetingImage from './GreetingImage.jsx'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Greeting/>
+    <GreetingImage/>
   </StrictMode>,
 )
