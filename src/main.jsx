0916ecfa142a5046df615dffb9c7ff0c0cb4789e
Damian Wujec <wujecdamian12@gmail.com>
2026-04-25 00:28:03 +0200

    feat: add Button1 and Button2 components to the main render

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import Greeting from './Greeting.jsx'
 import GreetingImage from './GreetingImage.jsx'
+import { Button1,Button2 } from './Buttons.jsx'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Greeting/>
     <GreetingImage/>
+    <Button1></Button1>
+    <Button2></Button2>
   </StrictMode>,
 )
