055bf5cd4bfd14a15cd2cff01e25d031017fa339
WujecDamian <wujecdamian12@gmail.com>
2026-04-25 22:50:46 +0200

    feat: add TodoList component to the main render

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Greeting from './Greeting.jsx'
+import TodoList from './JsInJSX.jsx'
+
 import GreetingImage from './GreetingImage.jsx'
 import { Button1,Button2 } from './Buttons.jsx'
 import App from './App.jsx'
@@ -9,6 +11,7 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Greeting/>
+    <TodoList/>
     <GreetingImage/>
     <Button1></Button1>
     <Button2></Button2>
