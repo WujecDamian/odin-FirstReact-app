f2ce522abce3bdcc128e5516091be1cecb1ae9a1
Damian Wujec <wujecdamian12@gmail.com>
2026-04-26 14:47:01 +0200

    feat: add Bio and TestJSX components to main render

--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,16 @@ import TodoList from './JsInJSX.jsx'
 
 import GreetingImage from './GreetingImage.jsx'
 import { Button1,Button2 } from './Buttons.jsx'
+import TestJSX from './JSXcomponent.jsx'
+import Bio from './JSXchallenge.jsx'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Greeting/>
     <TodoList/>
+    <Bio></Bio>
+    <TestJSX></TestJSX>
     <GreetingImage/>
     <Button1></Button1>
     <Button2></Button2>
