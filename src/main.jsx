6f3e33c83e6389275bf60b3bc81fd8428b2cef22
WujecDamian <wujecdamian12@gmail.com>
2026-04-27 21:22:47 +0200

    refactor: remove unused import of App component from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,6 @@ import GreetingImage from './GreetingImage.jsx'
 import { Button1,Button2 } from './Buttons.jsx'
 import TestJSX from './JSXcomponent.jsx'
 import Bio from './JSXchallenge.jsx'
-import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
