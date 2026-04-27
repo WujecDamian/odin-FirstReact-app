9eb2389dab613f9dedec7debd98d1d8921e1a43b
WujecDamian <wujecdamian12@gmail.com>
2026-04-27 21:23:05 +0200

    refactor: remove unused import of TestJSX component from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,6 @@ import TodoList from './JsInJSX.jsx'
 
 import GreetingImage from './GreetingImage.jsx'
 import { Button1,Button2 } from './Buttons.jsx'
-import TestJSX from './JSXcomponent.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
