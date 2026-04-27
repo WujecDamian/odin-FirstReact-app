49aacc86b1c0fe9a01d65c99cd4e6e87d92dbd3e
WujecDamian <wujecdamian12@gmail.com>
2026-04-27 21:22:54 +0200

    refactor: remove unused import of Bio component from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,6 @@ import TodoList from './JsInJSX.jsx'
 import GreetingImage from './GreetingImage.jsx'
 import { Button1,Button2 } from './Buttons.jsx'
 import TestJSX from './JSXcomponent.jsx'
-import Bio from './JSXchallenge.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
