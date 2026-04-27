124a3bbf22e5209a5018413176a62c4c1fa4c2d6
WujecDamian <wujecdamian12@gmail.com>
2026-04-27 21:23:33 +0200

    refactor: remove unused import of TodoList from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Greeting from './Greeting.jsx'
-import TodoList from './JsInJSX.jsx'
 
 
 createRoot(document.getElementById('root')).render(
