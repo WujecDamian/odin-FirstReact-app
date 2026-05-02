e6e3a55b4168a4b93d76ce65033ebdf2f5b55be6
Damian Wujec <wujecdamian12@gmail.com>
2026-05-02 21:07:53 +0200

    feat: render Person component within StrictMode in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Greeting from './Greeting.jsx'
-
+import Person from './Person.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Greeting/>
+    <Person/>
   </StrictMode>,
 )
