39ca92d98ea2daf71b0c02efc129d7f830691f85
Damian Wujec <wujecdamian12@gmail.com>
2026-05-12 18:47:39 +0200

    feat: replace Greeting and Person components with App component in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import Greeting from './Greeting.jsx'
-import Person from './Person.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Greeting/>
-    <Person/>
+    <App />
   </StrictMode>,
-)
+);
