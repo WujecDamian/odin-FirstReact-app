9d7a5cd4937a2ea7950a261768204f3e796806ae
Damian Wujec <wujecdamian12@gmail.com>
2026-05-12 18:49:20 +0200

    refactor: remove FunctionalInput component from App and clean up JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import "./App.css";
 import "./Style.css";
-import FunctionalInput from "./components/Class-based-components/FunctionalInput.jsx";
 import ClassInput from "./components/Class-based-components/ClassInput.jsx";
 function App() {
   return (
     <>
-      <FunctionalInput name="Functional component!" />
-      <div className="divider" />
       <ClassInput name="Class based component!" />
     </>
   );
